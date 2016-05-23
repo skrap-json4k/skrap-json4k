58a7bd9b437ec098c1429fce9867a0ee25fe9c77
yy_yank <yy.yank.dev.blog@gmail.com>
2016-05-23 23:30:03 +0900

    create new function header(TODO("body"))

--- src/main/kotlin/org/skrap/skrap4k/Skrap4K.kt
+++ src/main/kotlin/org/skrap/skrap4k/Skrap4K.kt
@@ -3,3 +3,14 @@ package main.kotlin.org.skrap.skrap4k
 /**
  * Created by yy_yank on 2016/05/21.
  */
+
+
+/**
+ * json文字列にJavaのクラスインスタンスに変換
+ */
+fun <T> jsonToObject(jsonString : String) : T = TODO("jsonからJava Object")
+
+/**
+ * Javaのクラスインスタンスをjson文字列に変換
+ */
+fun <T> objectToJson(obj : T) : T = TODO("Java Objectからjson")
