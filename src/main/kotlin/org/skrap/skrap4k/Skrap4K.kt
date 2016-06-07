068af19cfe9b5b5d6ec8a3f1feec2a396373a313
yy_yank <yy.yank.dev.blog@gmail.com>
2016-06-07 22:14:05 +0900

    uodate 2 functions

--- src/main/kotlin/org/skrap/skrap4k/Skrap4K.kt
+++ src/main/kotlin/org/skrap/skrap4k/Skrap4K.kt
@@ -1,16 +1,25 @@
 package main.kotlin.org.skrap.skrap4k
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import sun.jvm.hotspot.oops.Klass
+
 /**
  * Created by yy_yank on 2016/05/21.
  */
 
 
 /**
- * json文字列にJavaのクラスインスタンスに変換
+ * json文字列をクラスインスタンスに変換
  */
-fun <T> jsonToObject(jsonString : String) : T = TODO("jsonからJava Object")
+fun  <T> jsonToObject(jsonString : String, clazz : Class<T>) : T {
+    val mapper = ObjectMapper();
+    return mapper.readValue(jsonString, clazz)
+}
 
 /**
- * Javaのクラスインスタンスをjson文字列に変換
+ * クラスインスタンスをjson文字列に変換
  */
-fun <T> objectToJson(obj : T) : T = TODO("Java Objectからjson")
+fun <T> objectToJson(obj : T) : String {
+    val mapper = ObjectMapper();
+    return mapper.writeValueAsString(obj)
+}
